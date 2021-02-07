b63205c91ef32710c844dd1ef870c369f9f889e9
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2021-02-06 23:18:24 -0300

    feat: get some fixed news in case ibge api is off

---
+++ public/some-news.js
@@ -0,0 +1,222 @@
+export default {
+  conteudo: [
+    {
+      tipo: 'n',
+      id: 29974,
+      title:
+        'Produção industrial avança 0,9% em dezembro e fecha 2020 com queda de 4,5%',
+      alias:
+        'producao-industrial-avanca-0-9-em-dezembro-e-fecha-2020-com-queda-de-4-5',
+      catid: 2013,
+      publish_up: '2021-02-02 12:00:00',
+      language: 'pt-BR',
+      introtext:
+        'Em dezembro de 2020, a produção industrial cresceu 0,9% frente a novembro, na série com ajuste sazonal. Após oito meses de alta, o setor acumulou...',
+      produtoId: 0,
+      rota:
+        '2013-agencia-de-noticias/releases/29974-producao-industrial-avanca-0-9-em-dezembro-e-fecha-2020-com-queda-de-4-5.html'
+    },
+    {
+      tipo: 'n',
+      id: 29950,
+      title:
+        'De grandes obras a prevenção de catástrofes, conheça as funções das redes geodésicas',
+      alias:
+        'de-grandes-obras-a-prevencao-de-catastrofes-conheca-as-funcoes-das-redes-geodesicas',
+      catid: 2012,
+      publish_up: '2021-01-29 13:00:00',
+      language: 'pt-BR',
+      introtext:
+        'Definir as coordenadas paras obras de infraestrutura. Demarcar terras agrícolas. Detectar perturbações na atmosfera que possam interferir na...',
+      produtoId: 0,
+      rota:
+        '2012-agencia-de-noticias/noticias/29950-de-grandes-obras-a-prevencao-de-catastrofes-conheca-as-funcoes-das-redes-geodesicas.html'
+    },
+    {
+      tipo: 'n',
+      id: 29948,
+      title:
+        'Índice de Preços ao Produtor (IPP) varia 0,41% em dezembro e fecha 2020 em 19,40%',
+      alias:
+        'indice-de-precos-ao-produtor-ipp-varia-0-41-em-dezembro-e-fecha-2020-em-19-40',
+      catid: 2013,
+      publish_up: '2021-01-29 12:00:00',
+      language: 'pt-BR',
+      introtext:
+        'Os preços da indústria subiram 0,41% em dezembro, na comparação com novembro. Em dezembro, 17 das 24 atividades apresentaram variações positivas de...',
+      produtoId: 0,
+      rota:
+        '2013-agencia-de-noticias/releases/29948-indice-de-precos-ao-produtor-ipp-varia-0-41-em-dezembro-e-fecha-2020-em-19-40.html'
+    },
+    {
+      tipo: 'n',
+      id: 29934,
+      title:
+        'PNAD Contínua: taxa de desocupação é de 14,1% e taxa de subutilização é de 29,0% no trimestre encerrado em novembro de 2020',
+      alias:
+        'pnad-continua-taxa-de-desocupacao-e-de-14-1-e-taxa-de-subutilizacao-e-de-29-0-no-trimestre-encerrado-em-novembro-de-2020',
+      catid: 2013,
+      publish_up: '2021-01-28 12:00:00',
+      language: 'pt-BR',
+      introtext:
+        'A taxa de desocupação (14,1%) no trimestre de setembro a novembro de 2020 ficou estável em relação ao trimestre de junho a agosto (14,4%) e cresceu...',
+      produtoId: 0,
+      rota:
+        '2013-agencia-de-noticias/releases/29934-pnad-continua-taxa-de-desocupacao-e-de-14-1-e-taxa-de-subutilizacao-e-de-29-0-no-trimestre-encerrado-em-novembro-de-2020.html'
+    },
+    {
+      tipo: 'n',
+      id: 29927,
+      title: 'IPCA-15 fica em 0,78% em janeiro',
+      alias: 'ipca-15-fica-em-0-78-em-janeiro',
+      catid: 2013,
+      publish_up: '2021-01-26 12:00:00',
+      language: 'pt-BR',
+      introtext:
+        'O Índice Nacional de Preços ao Consumidor Amplo 15 (IPCA-15) cresceu 0,78% em janeiro, contra 1,06% em dezembro. É o maior resultado para um mês de...',
+      produtoId: 0,
+      rota:
+        '2013-agencia-de-noticias/releases/29927-ipca-15-fica-em-0-78-em-janeiro.html'
+    },
+    {
+      tipo: 'd',
+      id: 29945,
+      title:
+        'IBGE divulgará, em 4 de março, Estatísticas de Gênero: Indicadores sociais das mulheres no Brasil - 2ª edição',
+      alias:
+        'ibge-divulgara-em-4-de-marco-estatisticas-de-genero-indicadores-sociais-das-mulheres-no-brasil-2-edicao',
+      catid: 2476,
+      publish_up: '2021-01-28 17:06:40',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 0,
+      rota: ''
+    },
+    {
+      tipo: 'd',
+      id: 29923,
+      title:
+        'IBGE divulgará, em 25 de fevereiro às 10h, a Divisão Territorial Brasileira e a Malha de Setores Censitários, referentes a 2020',
+      alias:
+        'ibge-divulgara-em-25-de-fevereiro-as-10h-a-divisao-territorial-brasileira-e-a-malha-de-setores-censitarios-referentes-a-2020',
+      catid: 2476,
+      publish_up: '2021-01-25 12:00:00',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 0,
+      rota: ''
+    },
+    {
+      tipo: 'd',
+      id: 29922,
+      title:
+        'IBGE divulgará, em 23 de fevereiro às 10h, as Alterações Toponímicas dos Municípios referentes a 2020',
+      alias:
+        'ibge-divulgara-em-23-de-fevereiro-as-10h-as-alteracoes-toponimicas-dos-municipios-referentes-a-2020',
+      catid: 2476,
+      publish_up: '2021-01-22 12:00:00',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 0,
+      rota: ''
+    },
+    {
+      tipo: 'd',
+      id: 29814,
+      title:
+        'Atualização cadastral das pesquisas trimestrais da pecuária por telefone - SIMCAD AGRO',
+      alias:
+        'atualizacao-cadastral-das-pesquisas-trimestrais-da-pecuaria-por-telefone-simcad-agro',
+      catid: 2476,
+      publish_up: '2021-01-05 17:28:52',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 0,
+      rota: ''
+    },
+    {
+      tipo: 'd',
+      id: 29790,
+      title:
+        'Nova etapa de devolução da taxa de inscrição do concurso do Censo 2020',
+      alias:
+        'nova-etapa-de-devolucao-da-taxa-de-inscricao-do-concurso-do-censo-2020',
+      catid: 2476,
+      publish_up: '2021-01-04 13:00:00',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 0,
+      rota: ''
+    },
+    {
+      tipo: 'e',
+      id: 29939,
+      title:
+        'Correção dos valores de receita da PIA 2018 e das tabelas do SIDRA',
+      alias:
+        'correcao-dos-valores-de-receita-da-pia-2018-e-das-tabelas-do-sidra',
+      catid: 1998,
+      publish_up: '2021-01-28 12:31:50',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 9042,
+      rota: ''
+    },
+    {
+      tipo: 'e',
+      id: 29918,
+      title:
+        '    Erro de calendário na divulgação da Pesquisa Nacional por Amostra de Domicílios Trimestral - Período de referência: 10/2020 a 12/2020',
+      alias:
+        'erro-de-calendario-na-divulgacao-da-pesquisa-nacional-por-amostra-de-domicilios-trimestral-periodo-de-referencia-10-2020-a-12-2020',
+      catid: 1998,
+      publish_up: '2021-01-18 20:43:19',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 9173,
+      rota: ''
+    },
+    {
+      tipo: 'e',
+      id: 29916,
+      title:
+        'Correção do valor de consumo médio de fibras  para mulheres com 60 anos ou mais de idade, na tabela 15 da POF 2017-2018 – Análise do consumo alimentar pessoal no Brasil',
+      alias:
+        'correcao-do-valor-de-consumo-medio-de-fibras-para-mulheres-com-60-anos-ou-mais-de-idade-na-tabela-15-da-pof-2017-2018-analise-do-consumo-alimentar-pessoal-no-brasil',
+      catid: 1998,
+      publish_up: '2021-01-15 17:42:36',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 24786,
+      rota: ''
+    },
+    {
+      tipo: 'e',
+      id: 29751,
+      title:
+        'Substituição das tabelas 5464 e 5467 do SIDRA e das tabelas correspondentes em formato xls e ods da Pesquisa de Inovação (PINTEC) ',
+      alias:
+        'substituicao-das-tabelas-5464-e-5467-do-sidra-e-das-tabelas-correspondentes-em-formato-xls-e-ods-da-pesquisa-de-inovacao-pintec',
+      catid: 1998,
+      publish_up: '2020-12-21 17:04:00',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 9141,
+      rota: ''
+    },
+    {
+      tipo: 'e',
+      id: 29734,
+      title:
+        'Atraso na liberação no SIDRA dos dados do PIB dos Municípios 2018 ',
+      alias:
+        'atraso-na-liberacao-no-sidra-dos-dados-do-pib-dos-municipios-2018',
+      catid: 1998,
+      publish_up: '2020-12-16 16:28:55',
+      language: 'pt-BR',
+      introtext: '',
+      produtoId: 9088,
+      rota: ''
+    }
+  ]
+}
